04c8866a5e48c32f0bb3e26a28cf1c53b9c13f0e
moa-mel <52823454+moa-mel@users.noreply.github.com>
2023-05-18 13:52:18 +0100

    day five

---
+++ 05_Day_Lists/fifth.py
@@ -0,0 +1,48 @@
+empty_list = list()
+print(len(empty_list))
+
+fruits = ['banana', 'orange', 'mango', 'lemon']
+vegetables = ['Tomato', 'Potato', 'Cabbage', 'Onion', 'Carrot']
+animal_products = ['milk', 'meat', 'butter', 'yoghut']
+web_techs = ['HTML', 'CSS', 'JS', 'React', 'Redux', 'Node', 'MongoDB']
+countries = ['Finland', 'Estonia', 'Denmark', 'Sweden', 'Norway']
+
+print('Fruits:', fruits)
+print('Number of fruits:', len(fruits))
+print('Vegetables:', vegetables)
+print('Number of vegetables:', len(vegetables))
+print('Animal products:', animal_products)
+print('Number of animal products:', len(animal_products))
+print('Web technologies:', web_techs)
+print('Number of web technologies:', len(web_techs))
+print('Countries:', countries)
+print('Number of countries:', len(countries))
+
+fruits = ['banana', 'orange', 'mango', 'lemon']
+first_fruit = fruits[0]
+print(first_fruit)
+second_fruit =fruits[1]
+print(second_fruit)
+last_fruit =fruits[3]
+print(last_fruit)
+last_index = len(fruits)-1
+last_fruit = fruits[last_index]
+
+fruits = ['banana', 'orange', 'mango', 'lemon']
+last_fruit = fruits[-1]
+second_last = fruits[-2]
+print(last_fruit)
+print(second_last)
+
+#slicing items
+fruits = ['banana', 'orange', 'mango', 'lemon']
+all_fruits = fruits[0:4]
+print(all_fruits)
+all_fruits = fruits[0:]
+print(all_fruits)
+some_fruits = fruits[:3]
+print(some_fruits)
+orange_and_mango = fruits[1:3]
+print(orange_and_mango)
+orange_mango_lemon = fruits[-3:]
+print(orange_mango_lemon)
