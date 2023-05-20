89e294ff8ca2ac4b6f3127d724402f68045fa684
moa-mel <52823454+moa-mel@users.noreply.github.com>
2023-05-20 19:19:30 +0100

    move to day 6

--- 05_Day_Lists/fifth.py
+++ 05_Day_Lists/fifth.py
@@ -84,3 +84,61 @@ print(fruits)
 fruits = ['banana', 'orange', 'mango', 'lemon']
 fruits.clear()
 print(fruits)
+
+fruits = ['banana', 'orange', 'mango', 'lemon']
+fruits_copy = fruits.copy()
+print(fruits_copy)
+#join 
+positive_numbers = [1, 2, 3, 4, 5]
+zero = [0]
+negative_numbers = [-5, -4, -3, -2, -1]
+integers = negative_numbers + zero + positive_numbers
+print(integers)
+fruits = ['banana', 'orange', 'mango', 'lemon']
+vegetables = ['Tomato', 'Potato', 'Cabbage','Onion', 'Carrot']
+fruits_and_vegetables = fruits + vegetables
+print(fruits_and_vegetables)
+# join with extend
+num1 = [0, 1, 2, 3]
+num2 = [4, 5, 6]
+num1.extend(num2)
+print('Numbers:', num1)
+negative_numbers = [-5,-4,-3,-2,-1]
+positive_numbers = [1, 2, 3,4,5]
+zero = [0]
+
+negative_numbers.extend(zero)
+negative_numbers.extend(positive_numbers)
+print('Integers:', negative_numbers)
+fruits = ['banana', 'orange', 'mango', 'lemon']
+vegetables = ['Tomato', 'Potato', 'Cabbage','Onion', 'Carrot'] 
+fruits.extend(vegetables)
+print('Fruits and vegetablesb:', fruits)
+#count
+fruits = ['banana', 'orange', 'mango', 'lemon']
+print(fruits.count('orange'))
+ages = [22, 19, 24, 25, 26, 24, 25, 24]
+print(ages.count(24))
+#index
+fruits = ['banana', 'orange', 'mango', 'lemon']
+print(fruits.index('orange'))
+ages = [22, 19, 24, 25, 26, 24, 25, 24]
+print(ages.index(24))
+fruits = ['banana', 'orange', 'mango', 'lemon']
+fruits.reverse()
+print(fruits)
+ages = [22, 19, 24, 25, 26, 24, 25, 24]
+ages.reverse()
+print(ages)
+
+#sort
+fruits = ['banana', 'orange', 'mango', 'lemon']
+fruits.sort()
+print(fruits)
+fruits.sort(reverse=True)
+print(fruits)
+ages = [22, 19, 24, 25, 26, 24, 25, 24]
+ages.sort()
+print(ages)
+ages.sort(reverse=True)
+print(ages)
